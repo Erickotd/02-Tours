e44b36c4efee237df53027717322e40704f29907
erickotd <erickotd@gmail.com>
2020-10-31 22:53:49 +0100

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
-import Tours from './Tourss';
+import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 const App = () => {
